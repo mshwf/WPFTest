b9747f165c906647e080fb49fe7c6cd4a0eadb66
mshwf <m_shawaf@outlook.com>
2018-02-20 18:01:16 +0200

    trying getting Item Text by Expressions

---
+++ WPFTest/Converters/PropertyMultiValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFTest.Converters
+{
+    class PropertyMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            object selectedItem = values[0];
+            string displayMemberPath = values[1] as string;
+
+            if (selectedItem != null)
+            {
+                ParameterExpression param = Expression.Parameter(selectedItem.GetType(), "x");
+                MemberExpression body = Expression.Property(param, displayMemberPath);
+                LambdaExpression lambda = Expression.Lambda(body, param);
+                Delegate expression = lambda.Compile();
+
+                var val = expression.DynamicInvoke(selectedItem);
+                return val;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
